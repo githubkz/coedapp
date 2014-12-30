cc6d88bf8b79c0f7c0f86455a4cf7ead3874594c
kz-kasai <rgb.cmyk.iroiro@gmail.com>
2014-12-30 15:31:35 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoedApp::Application.config.secret_key_base = 'a787303c237f77e29dfcb1ad3b4ed4abff6557390f5bc84da94104c9b37e3317abdcdd162455bf7aff3fc3cb19abcd86586a889cc98bef50b17ecc6c1659b947'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoedApp::Application.config.session_store :cookie_store, key: '_CoedApp_session'
